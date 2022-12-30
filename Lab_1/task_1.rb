31df915f8c10348a743cdb3afa1341715680907c
kirill <kirilltitoff@mail.ru>
2022-12-30 23:20:06 +0300

    пункт 1.2

--- Lab_1/task_1.rb
+++ Lab_1/task_1.rb
@@ -1,1 +1,24 @@
 puts "Hello world"
+
+##################
+
+input_name = ARGV[0]
+puts "Категорически приветствую, #{input_name}!"
+
+input = STDIN.gets
+name = input.chomp
+puts "#{name}, какой твой любимый язык?"
+
+lang = STDIN.gets.chomp
+case lang
+when "ruby","Ruby"
+  puts "Ты подлиза!"
+when "python","Python"
+  puts "Мое почтение!"
+when "c++","C++"
+  puts "Перваш"
+when "Prolog","prolog"
+  puts "Ты в своем познании настолько преисполнился"
+else
+  "Вау, что-то неординарное"
+end
