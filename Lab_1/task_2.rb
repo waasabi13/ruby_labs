bd4e0a6f0c7208310e1989efaff10b5eb7a0ef47
kirill <kirilltitoff@mail.ru>
2022-12-31 18:02:48 +0300

    задание 2.1

---
+++ Lab_1/task_2.rb
@@ -0,0 +1,30 @@
+#1
+
+def is_simple(x)
+  result=true
+  if x==1
+    result=false
+  end
+  if x==2
+    result=true
+  end
+  cur_del=2
+  while cur_del<x
+    if x%cur_del==0
+      result=false
+    end
+    cur_del+=1
+  end
+  result
+end
+def sum_digits(x)
+  sum=0
+  cur_del=2
+  while cur_del<x
+    if x%cur_del==0 and is_simple(cur_del)==true
+      sum+=cur_del
+    end
+    cur_del+=1
+  end
+  p sum
+end
