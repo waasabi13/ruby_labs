f040598e673ad3a83744ab627e445316581bb9cb
kirill <kirilltitoff@mail.ru>
2023-02-02 15:25:16 +0300

    реализация выбора номера метода как второго аргумента(задание 3.2)

--- Lab_1/task3/task_3.rb
+++ Lab_1/task3/task_3.rb
@@ -18,11 +18,19 @@ def find_first_positive_element(array)
   -1
 end
 
-
-
 file_path = ARGV[0]
 file = File.open(file_path)
 array = file.readline.split(' ').map(&:to_i)
 puts "Массив: #{array}"
 puts "Поиск минимального элемента: #{find_min(array)}"
 puts "Поиск индекса первого положительного элемента: #{find_first_positive_element(array)}"
+
+number_method = ARGV[1].to_i
+methods_array = [:find_min, :find_first_positive_element] # массив, состоящий из символов
+# вдохновился с https://www.ruby-lang.org/ru/documentation/ruby-from-other-languages/
+
+unless number_method.between?(0, methods_array.length - 1)
+  puts 'Некорректный номер вызываемого метода'
+  return
+end
+puts "Результат работы вызываемого метода: #{method(methods_array[number_method]).call(array)}"
