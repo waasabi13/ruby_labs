31fb5a7a9b99200cb7b4b62bdd5f472a2ff2b86e
kirill <kirilltitoff@mail.ru>
2023-03-10 18:34:08 +0300

    Создание функции write_to_txt в main + создание файла, содержащего объекты студентов

--- Lab_2/main.rb
+++ Lab_2/main.rb
@@ -19,4 +19,22 @@ def read_from_txt(file_path)
   students_list
 end
 
+def write_to_txt(file_path, student_list)
+  result = '{"StudentList":['
+  student_list.each do |student|
+    result += student.to_json + ","
+  end
+  result = result.chop + "]}"
+  File.write(file_path, result)
+end
+
+student1 = Student.new('Полетов', 'Разбор', 'Алексеевич')
+student2 = Student.new('Пиндосов', 'Облом', 'Баракович', { id: 1, telegram: '@fakk_usa' })
+student3 = Student.new('Атому', 'Ли', 'Ядала', { phone: '79181461800', email: 'goaloffway@mail.ru', git: '@vitaliyg' })
+student4 = Student.new('Гераклов', 'Поход', 'Подвигоевич', { id: 12, phone: '79698876534' })
+student5 = Student.new('Братанов', 'Друган', 'Кентович', { id: 77, phone: '+7 (777)-777-77-77' })
+
+student_list = [student1, student2, student3, student4, student5]
+
 puts read_from_txt('/Users/kirilltitov/RubymineProjects/ruby_labs/Lab_2/student_list.txt')
+write_to_txt('/Users/kirilltitov/RubymineProjects/ruby_labs/Lab_2/new_student_list.txt', student_list)
