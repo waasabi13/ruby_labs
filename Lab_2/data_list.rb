30d2f2605adf28c51c1ac430f1d6d34b4df9d143
kirill <kirilltitoff@mail.ru>
2023-03-17 15:41:54 +0300

    реализация методов класса datalist(лаба 2 задание 3.4)

--- Lab_2/data_list.rb
+++ Lab_2/data_list.rb
@@ -2,10 +2,40 @@
 
 class DataList
 
-  def initialize(list)
-    self.list = list
+  private_class_method :new
+
+
+
+  def initialize(objects)
+    self.objects_list = objects
+    self.selected_objects = []
   end
 
+  def select(number)
+    selected_objects.append(number)
+  end
+
+  def clear_select
+    self.selected_objects = []
+  end
+
+  def get_selected
+    return [] if selected_objects.empty?
+
+    selected_id_list = []
+    selected_objects.each do |num|
+      selected_id_list.append(objects_list[num].id)
+    end
+    selected_id_list
+  end
+
+  protected
+
+  def get_names; end
+
+  def get_data; end
+
   private
-  attr_accessor :list
+
+  attr_accessor :selected_objects, :objects_list
 end
