4a797c59abb7979538c3f3c1a42c0944f6561f4c
kirill <kirilltitoff@mail.ru>
2023-04-07 14:30:33 +0300

    Выполнение select запроса из ruby

--- LabStudents/main.rb
+++ LabStudents/main.rb
@@ -1,7 +1,8 @@
 require_relative 'models/student'
 require_relative 'models/student_short'
 require_relative 'repositories/containers/data_table'
-
+require 'yaml'
+require 'sqlite3'
 def read_from_txt(file_path)
   raise ArgumentError, 'File not found' unless File.exist?(file_path)
 
@@ -29,22 +30,7 @@ def write_to_txt(file_path, student_list)
   File.write(file_path, result)
 end
 
-# student1 = Student.new('Полетов', 'Разбор', 'Алексеевич')
-# student2 = Student.new('Пиндосов', 'Облом', 'Баракович', { id: 1, telegram: '@fakk_usa' })
-# student3 = Student.new('Атому', 'Ли', 'Ядала', {email: 'goaloffway@mail.ru', git: '@vitaliyg' })
-# student4 = Student.new('Гераклов', 'Поход', 'Подвигоевич', { id: 12, phone: '79698876534' })
-# student5 = Student.new('Братанов', 'Друган', 'Кентович', { id: 77, phone: '+7 (777)-777-77-77' })
-#
-# student_list = [student1, student2, student3, student4, student5]
-#
-# #puts read_from_txt('/Users/kirilltitov/RubymineProjects/ruby_labs/LabStudents/student_list.txt')
-# write_to_txt('/Users/kirilltitov/RubymineProjects/ruby_labs/LabStudents/new_student_list.txt', student_list)
-#
-# st_test = StudentShort.new(student3)
-#
-# puts st_test.contact
-
-test = [[1, 'Test'], [2, 'tesT']]
-test_table = DataTable.new(test)
-puts test_table
-puts test_table.get_item(0, 1)
+db = SQLite3::Database.open './university.sqlite'
+sel = db.prepare "Select * from students"
+results = sel.execute
+results.each { |row| puts row.join "\s" }
